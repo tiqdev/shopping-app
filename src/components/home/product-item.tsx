22bd781c59b1e809fcc7b9ddec86e8bc8c6a1ec8
Tarık KAYA <tiqqq24@gmail.com>
2024-05-10 20:01:44 +0300

    Refactor product item component and update grid layout

--- src/components/home/product-item.tsx
+++ src/components/home/product-item.tsx
@@ -6,7 +6,7 @@ import { MouseEvent } from "react";
 const ProductItem = ({ product }: { product: Product }) => {
   return (
     <Link to={"/detail/" + product.id} className="center-col" key={product.id}>
-      <div className="flex flex-col gap-3 items-stretch p-[10px] bg-white h-fit w-[180px] border border-card rounded-[4px] hover:shadow-md transition-shadow">
+      <div className="flex flex-col gap-3 items-stretch p-[10px] bg-white h-fit mobile:w-[180px] w-[150px] border border-card rounded-[4px] hover:shadow-md transition-shadow">
         <img
           src={product.image}
           alt={product.name}
@@ -16,7 +16,7 @@ const ProductItem = ({ product }: { product: Product }) => {
         <span className="text-primary font-medium text-sm">
           {product.price} ₺
         </span>
-        <span className="leading-5 font-medium text-balance h-10">
+        <span className="leading-5 text-base font-medium text-balance h-10">
           {product.name}
         </span>
 
--- src/components/home/skeleton-product.tsx
+++ src/components/home/skeleton-product.tsx
@@ -1,10 +1,10 @@
 const SkeletonProduct = () => {
   return (
     <div className="center-col">
-      <div className="animate-pulse flex flex-col gap-[15px] items-stretch p-[10px] bg-white h-fit w-[180px]">
-        <div className="w-[160px] h-[150px] bg-gray-300"></div>
+      <div className="animate-pulse flex flex-col gap-3 items-stretch p-[10px] bg-white h-fit mobile:w-[180px] w-[150px] border border-card rounded-[4px] hover:shadow-md transition-shadow">
+        <div className="mobile:w-[160px] w-[128px] h-[150px] bg-gray-300"></div>
         <div className="w-[100px] h-5 bg-gray-300"></div>
-        <div className="w-[150px] h-5 bg-gray-300"></div>
+        <div className="mobile:w-[150px] w-[128px] h-5 bg-gray-300"></div>
         <div className="w-[100px] h-5 bg-gray-300"></div>
       </div>
     </div>
